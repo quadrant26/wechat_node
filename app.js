2c5d823828b70a57d56bc3f9e34c2a46119a0f05
quadrant26 <quadrant26@163.com>
2017-04-27 23:07:44 +0800

    repair err

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var wechat = require("./wechat/g")
 var wechat_file = path.join(__dirname, './config/wechat.txt')
 var config = {
     wechat: {
-        appId : "wx83ab7dc18148dc9b",
+        appID : "wx83ab7dc18148dc9b",
         appSecret : "4594ac21d2c47e5721f61db9b6196c7e",
         token : "supernova",
-        getAccessToken : function (data){
+        getAccessToken: function (){
             return util.readFileAsync(wechat_file)
         },
-        setAccessToken : function (data){
+        saveAccessToken: function (data){
             var data = JSON.stringify(data);
             return util.writeFileAsync(wechat_file, data)
         }
